3abc061fc8b8678efc200a51f1813a029ea62203
NipunPrabushitha <nprabushitha@gmail.com>
2024-11-05 01:21:57 +0530

    add forgetPassword ui and controller

---
+++ src/main/java/org/example/controller/ForgetPasswordController.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class ForgetPasswordController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private PasswordField txtNewPassword;
+
+    @FXML
+    private TextField txtQuestion;
+
+    @FXML
+    private PasswordField txtReEnterPassword;
+
+    @FXML
+    void btnConfirmOnAction(ActionEvent event) {
+
+    }
+
+}
