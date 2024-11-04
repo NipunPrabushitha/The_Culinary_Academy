cc0a4ab805bff56e25c74813c3ba599b731b7243
NipunPrabushitha <nprabushitha@gmail.com>
2024-11-05 01:49:05 +0530

    setup forgetPassword

--- src/main/java/org/example/controller/ForgetPasswordController.java
+++ src/main/java/org/example/controller/ForgetPasswordController.java
@@ -2,9 +2,14 @@ package org.example.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ForgetPasswordController {
 
@@ -22,7 +27,21 @@ public class ForgetPasswordController {
 
     @FXML
     void btnConfirmOnAction(ActionEvent event) {
-
+        navigateToLogIn();
     }
+    public void navigateToLogIn()  {
+        AnchorPane rootNode = null;
+        try {
+            rootNode = FXMLLoader.load(this.getClass().getResource("/view/Login.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Dashboard Form");
+    }
 }
--- src/main/java/org/example/controller/LoginController.java
+++ src/main/java/org/example/controller/LoginController.java
@@ -2,9 +2,14 @@ package org.example.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
 
@@ -23,7 +28,21 @@ public class LoginController {
     }
     @FXML
     void forgetPasswordOnAction(ActionEvent event) {
-
+        navigateToForgotPassword();
     }
+    public void navigateToForgotPassword()  {
+        AnchorPane rootNode = null;
+        try {
+            rootNode = FXMLLoader.load(this.getClass().getResource("/view/forgetPassword.fxml"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
+        Scene scene = new Scene(rootNode);
+
+        Stage stage = (Stage) this.root.getScene().getWindow();
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.setTitle("Dashboard Form");
+    }
 }
