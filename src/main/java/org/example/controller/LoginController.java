4d1fd00f6dee113b642b7aeafa4422e1d4e85aad
NipunPrabushitha <nprabushitha@gmail.com>
2024-11-04 22:57:23 +0530

    init the project

---
+++ src/main/java/org/example/controller/LoginController.java
@@ -0,0 +1,25 @@
+package org.example.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginController {
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TextField txtAdminId;
+
+    @FXML
+    private PasswordField txtPasswoard;
+
+    @FXML
+    void btnLoginInOnAction(ActionEvent event) {
+
+    }
+
+}
